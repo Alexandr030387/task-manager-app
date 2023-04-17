cb51ec44bd5090361b2653b9849c314a9ae2f455
Alexander Pogrebnyak <alexander.pogrebnyak@sigma.software>
2023-04-17 15:42:26 +0300

    Update code style

--- src/routers/task.js
+++ src/routers/task.js
@@ -4,11 +4,11 @@ const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/tasks', auth, async (req, res) => {
-    // const task = new Task(req.body);
     const task = new Task({
         ...req.body,
         owner: req.user._id
     });
+
     try {
         await task.save()
 
